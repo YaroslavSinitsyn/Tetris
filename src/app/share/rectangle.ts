6aa7bd9bb6120e23256f3db4852f3b704394c882
y.synytsyn <y.synytsyn@temabit.com>
2017-04-02 12:06:41 +0300

    some fix

--- src/app/share/rectangle.ts
+++ src/app/share/rectangle.ts
@@ -3,7 +3,7 @@ export class Rectangle {
     public id: string = '0.0', 
     public x: number = 1, 
     public y: number = 1, 
-    public width: number = 25, 
-    public height: number = 25,
+    public width: number = 20, 
+    public height: number = 20,
     public color:string = '#fff') {}
 }
