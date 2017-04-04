00163d1dd4a629d7a1fa86787ff50d5fa84ecf3e
Yaroslav Synytsyn <sinitsyn.ya@gmail.com>
2017-04-04 07:21:00 +0300

    refactoring css, html

--- src/app/share/rectangle.ts
+++ src/app/share/rectangle.ts
@@ -3,7 +3,7 @@ export class Rectangle {
     public id: string = '0.0', 
     public x: number = 1, 
     public y: number = 1, 
-    public width: number = 20, 
-    public height: number = 20,
+    public width: number = 26, 
+    public height: number = 26,
     public color:string = '#fff') {}
 }
